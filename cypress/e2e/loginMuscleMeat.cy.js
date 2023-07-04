56b6ead9657ffb61f77a5c6611e03e9ffdb1135a
Jeffrey Jongkees <jeffrey.jongkees@mn.nl>
2023-07-04 13:29:53 +0200

    Final commit. Regarding updating the test steps

--- cypress/e2e/loginMuscleMeat.cy.js
+++ cypress/e2e/loginMuscleMeat.cy.js
@@ -3,11 +3,7 @@ import Login from '../fixtures/Pages/login.cy.js'
 
 describe('Muscle Meat', () => {
     it('Inloggen', () => {
-<<<<<<< HEAD
-        //cy.viewport(1624, 1000)
-=======
         cy.viewport(1624, 1000)
->>>>>>> a0fa443ea5462b65738d3a0791580e3ef6facd73
         cy.visit('https://musclemeat.nl') 
         Login.clickMyAccount();
         Login.fillInCredentials('jeffreyjongkees@gmail.com', 'Mm_123smok3y');
